6723600f87780c31ec6a9eb466a99eed49966b60
ivan <kulikovia.72@icloud.com>
2022-02-15 12:36:29 +0200

    add Tweak.Key

--- Sources/Tweak/Tweak.swift
+++ Sources/Tweak/Tweak.swift
@@ -1,16 +1,30 @@
 public struct Tweak {
     public init(
         id: ID,
-        args: [String: Any]
+        args: [Key: Any]
     ) {
         self.id = id
         self.args = args
     }
 
     public let id: ID
-    public let args: [String: Any]
+    public let args: [Key: Any]
     
     public struct ID: RawRepresentable, ExpressibleByStringLiteral {
+        public typealias StringLiteralType = String
+
+        public var rawValue: String
+
+        public init?(rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        public init(stringLiteral value: String) {
+            self.rawValue = value
+        }
+    }
+
+    public struct Key: Hashable, RawRepresentable, ExpressibleByStringLiteral {
         public typealias StringLiteralType = String
 
         public var rawValue: String
